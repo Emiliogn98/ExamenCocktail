b847fe60dc09e653eeaf9e949b59b721ca8a9185
Emiliogn98 <Emiliogn.98@gmail.com>
2023-06-16 10:51:24 -0600

    Vista Favoritos

---
+++ ExamenCocktail/Controller/GetFavoritosController.swift
@@ -0,0 +1,32 @@
+//
+//  GetFavoritosController.swift
+//  ExamenCocktail
+//
+//  Created by MacBookMBA4 on 16/06/23.
+//
+
+import UIKit
+
+class GetFavoritosController: UIViewController {
+    
+    
+    @IBOutlet weak var btnBuscarOutlet: UIButton!
+    
+    
+    @IBOutlet weak var txtBuscar: UITextField!
+    
+    
+    @IBOutlet weak var collectionViewFavoritos: UICollectionView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    @IBAction func btnBuscar(_ sender: Any) {
+    }
+    
+
+}
