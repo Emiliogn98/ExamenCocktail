a0706c1aba97cddc67052104bb73393369ef846f
EAguilar99 <e.agu.hndz@gmail.com>
2023-06-16 17:03:28 -0600

    union de los elementos al controlador favoritos details controller

--- ExamenCocktail/Controller/FavoritosDetailController.swift
+++ ExamenCocktail/Controller/FavoritosDetailController.swift
@@ -9,21 +9,33 @@ import UIKit
 
 class FavoritosDetailController: UIViewController {
 
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var lblNombre: UILabel!
+    
+    @IBOutlet weak var lblIngrediente1: UILabel!
+    
+    @IBOutlet weak var lblCantidad1: UILabel!
+    
+    @IBOutlet weak var lblIngrediente2: UILabel!
+    
+    @IBOutlet weak var lblCantidad2: UILabel!
+    
+    
+    @IBOutlet weak var lblIngrediente3: UILabel!
+    
+    @IBOutlet weak var lblCantidad3: UILabel!
+    
+    @IBOutlet weak var imagenView1: UIImageView!
+    @IBOutlet weak var imageView2: UIImageView!
+    @IBOutlet weak var imageView3: UIImageView!
+    
+    @IBOutlet weak var btnFavoritosOutlet: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
