3968dc895e739c539acfc1eac3f6954de3d42732
Emiliogn98 <Emiliogn.98@gmail.com>
2023-06-15 19:09:52 -0600

    Arquitectura MVVM

---
+++ ExamenCocktail/Model/Drink.swift
@@ -0,0 +1,8 @@
+//
+//  Drink.swift
+//  ExamenCocktail
+//
+//  Created by MacBookMBA4 on 15/06/23.
+//
+
+import Foundation
---
+++ ExamenCocktail/ViewModel/DrinkViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  DrinkViewModel.swift
+//  ExamenCocktail
+//
+//  Created by MacBookMBA4 on 15/06/23.
+//
+
+import Foundation
